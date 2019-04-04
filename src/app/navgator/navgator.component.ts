6c730e507c9fbb531ca334ea8c4ace56792ccd78
kaaden <794192675@qq.com>
2019-04-04 14:43:28 +0800

    add uppage

--- src/app/navgator/navgator.component.ts
+++ src/app/navgator/navgator.component.ts
@@ -1,35 +1,36 @@
-import { Component, OnInit, HostListener } from '@angular/core';
-import { Router, } from '@angular/router';
+import { Component, OnInit, HostListener } from "@angular/core";
+import { Router } from "@angular/router";
 @Component({
-  selector: 'app-navgator',
-  templateUrl: './navgator.component.html',
-  styleUrls: ['./navgator.component.less'],
-
+  selector: "app-navgator",
+  templateUrl: "./navgator.component.html",
+  styleUrls: ["./navgator.component.less"]
 })
 export class NavgatorComponent implements OnInit {
-  isDon: boolean = false
-  nav: string[] = ["HOME", "ABOUT", "TAG"]
-  subscribeScoll: any
-  constructor(private router: Router) { }
+  isDon: boolean = false;
+  nav: string[] = ["HOME", "ABOUT", "TAG"];
+  subscribeScoll: any;
+  constructor(private router: Router) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
+
   goPath(item) {
     let page = {
-      "HOME": "",
-      "ABOUT": "about",
-      "TAG": "tag"
-    }
-    this.router.navigate([page[item]])
+      HOME: "",
+      ABOUT: "about",
+      TAG: "tag"
+    };
+    this.router.navigate([page[item]]);
   }
   @HostListener("window:scroll", ["$event"])
   onScroll(e) {
     let scrollTop = document.documentElement.scrollTop;
-    let bgHeight = document.getElementById("head-bg") ? document.getElementById("head-bg").clientHeight : 0
-    if (scrollTop > bgHeight) {
-      this.isDon = true
+    let bgHeight = document.getElementById("head-bg")
+      ? document.getElementById("head-bg").clientHeight
+      : 0;
+    if (scrollTop >= bgHeight) {
+      this.isDon = true;
     } else {
-      this.isDon = false
+      this.isDon = false;
     }
   }
 }
